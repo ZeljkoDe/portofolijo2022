440b7afc626a85356e964a7f8fb87c4b3d60a328
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-01-22 10:05:12 +0100

    fresh start

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 "use strict";
 
-const clickMeBtn = document.querySelector('.js-click-me');
-
-if (clickMeBtn) {
-	clickMeBtn.addEventListener('click', function () {
-		this.classList.toggle('active');
-
-		this.classList.contains('active') ? document.body.style.backgroundColor = 'white' : document.body.style.backgroundColor = 'black';
-	});
-}
-// const animationObject = document.querySelector('.animation');
-
-// const scroll = () => {
-// 	const page = window.pageYOffset / 10;
-// 	animationObject.style.borderRadius = page + 'px';
-// };
-
-// document.addEventListener('scroll', scroll);
+console.log('test');
