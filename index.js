d59ac42edcd010af0dee67880b649217b96d0042
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-01-24 15:53:12 +0100

    maybe a start?

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 "use strict";
 
+const hiddenWorld = document.querySelector('.hidden_world');
+const playArea = document.querySelector('body');
+
+const theBeginingOfItAll = () => {
+	playArea.classList.add('white__magic');
+};
+
+hiddenWorld.addEventListener('mouseenter', theBeginingOfItAll);
+
 console.log('test');
