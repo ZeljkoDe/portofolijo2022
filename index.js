545a558988afc10110f6371e8e4acfd5768f5976
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-01-26 16:19:33 +0100

    light switch

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 "use strict";
 
-const hiddenWorld = document.querySelector('.hidden_world');
+// const hiddenWorld = document.querySelector('.hidden_world');
 const playArea = document.querySelector('body');
+const turnMeOn = document.querySelector('.switch');
 
 const theBeginingOfItAll = () => {
 	playArea.classList.add('white__magic');
 };
 
-hiddenWorld.addEventListener('mouseenter', theBeginingOfItAll);
+const zigibigul = function () {
+	console.log(this);
+	this.classList.toggle('off');
+};
+
+// hiddenWorld.addEventListener('mouseenter', theBeginingOfItAll);
+turnMeOn.addEventListener('click', zigibigul);
 
 console.log('test');
